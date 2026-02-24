8cc701b9a3b487157d806c1ce81075409cfd46df
Devesh Shukla <devshukla291698@gmail.com>
2026-02-24 18:19:05 +0530

    v1.0.4 updated to class based structure

--- src/script.js
+++ src/script.js
@@ -5,8 +5,7 @@ let date = new Date();
 $('.currYear').html(date.getFullYear());
 
 
-
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+// const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = $('.form');
 const containerWorkouts = $('.workouts');
@@ -21,73 +20,94 @@ const actElevation = $('.act_elevation');
 
 let map, mapEvent;
 
-// Geolocation API
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-        let { latitude } = position.coords;
-        let { longitude } = position.coords;
 
-        let coordsArr = [latitude, longitude];
+class App {
+    constructor() {
+        this.getPosition();
+        form.on('submit', this.newWorkout);
 
-        map = L.map('map').setView(coordsArr, 16); // Zoom level
+        // Change activity metric based on activity type
+        inputType.on('change', this.toggleElevationField);
+    }
 
+    getPosition() {
+        // Geolocation API
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                let { latitude } = position.coords;
+                let { longitude } = position.coords;
+
+                let coordsArr = [latitude, longitude];
+                this.loadMap(coordsArr);
+
+            }, () => {
+                alert('Unable to get your location!');
+            });
+        }
+    }
+
+    loadMap(coordinates) {
+        map = L.map('map').setView(coordinates, 16); // Zoom level
+
+        // Leaflet map
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
         // Handle map click
-        map.on('click', (mapE) => {
-            // Show event form first
-            mapEvent = mapE;
-            form.removeClass('hidden');
-            inputDistance.focus();
-        });
-    }, () => {
-        alert('Unable to get your location!');
-    });
-}
+        map.on('click', this.showForm);
+    }
 
-form.submit(function (e) { 
-    e.preventDefault();
-    
-    // Clear form
-    clearForm();
-    
-    // Display marker
-    const { lat, lng } = mapEvent.latlng;
+    showForm(mapE) {
+        // Show event form first
+        mapEvent = mapE;
+        form.removeClass('hidden');
+        inputDistance.focus();
+    }
 
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                riseOnHover: true,
-                autoClose: false,
-                closeOnEscapeKey: false,
-                closeOnClick: false,
-                className: 'tracking-popup',
-            })
-        )
-        .setPopupContent('Tracking Event')
-        .openPopup(); 
-});
+    toggleElevationField(e) {
+        const isRunning = e.target.value === 'running' || e.target.value === 'tracking';
+
+        const isCycling = e.target.value === 'cycling';
+
+        // Using Tag + Class for precise selection
+        // .toggleClass('className', switch) adds class if switch is true, removes if false
+        $('div.act_cadence').toggleClass('form__row--hidden', !isRunning);
+        
+        $('div.act_elevation').toggleClass('form__row--hidden', !isCycling);
+    }
+
+    newWorkout(e) {
+        e.preventDefault();
+            
+        // Clear form
+        clearForm();
+        
+        // Display marker
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    riseOnHover: true,
+                    autoClose: false,
+                    closeOnEscapeKey: false,
+                    closeOnClick: false,
+                    className: 'tracking-popup',
+                })
+            )
+            .setPopupContent('Tracking Event')
+            .openPopup();
+    }
+};
+
+const app = new App();
 
 // Clear form & hide
 const clearForm = () => {
     $('.form')[0].reset();
     form.addClass('hidden');
 }
-
-// Change activity metric based on activity type
-inputType.on('change', function (e) { 
-    const isRunning = e.target.value === 'running' || e.target.value === 'tracking';
-
-    const isCycling = e.target.value === 'cycling';
-
-    // Using Tag + Class for precise selection
-    // .toggleClass('className', switch) adds class if switch is true, removes if false
-    $('div.act_cadence').toggleClass('form__row--hidden', !isRunning);
-    
-    $('div.act_elevation').toggleClass('form__row--hidden', !isCycling);
-});
